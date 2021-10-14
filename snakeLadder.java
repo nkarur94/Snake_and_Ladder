3392c7d7e1250fdcf19f60f1245eaba49af5d764
nithunkarur <rknithun@gmail.com>
2021-10-14 13:19:44 +0530

    Added: Playersname and players start position

--- snakeLadder.java
+++ snakeLadder.java
@@ -3,5 +3,9 @@ class snakeLadder{
 	{
 		System.out.println("welcome to SNAKE and LADDER Game");
 		System.out.println("********************************");
+		String playerName="Nithun";
+		int playerPosition=0;
+		System.out.println("Welcome\t"+playerName+"\t*LETS START THE GAME* Your initial postion is:"+playerPosition);
+		System.out.println("---------------------------------------------------------------------------------------");
 	}
 }
