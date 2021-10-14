1a5927bd76b7b87c8c2c08b81e2d3145dac32a53
nithunkarur <rknithun@gmail.com>
2021-10-14 12:13:24 +0530

    Added: Welcome note

---
+++ snakeLadder.java
@@ -0,0 +1,7 @@
+class snakeLadder{
+	public static void main(String[] args)
+	{
+		System.out.println("welcome to SNAKE and LADDER Game");
+		System.out.println("********************************");
+	}
+}
