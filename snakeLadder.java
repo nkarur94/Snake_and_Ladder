bba5cbe3b493664cc9476f491cdae6980486e387
nithunkarur <rknithun@gmail.com>
2021-10-14 19:24:30 +0530

    Included: Used for case to Repeate till user reaches 100th position

--- snakeLadder.java
+++ snakeLadder.java
@@ -5,41 +5,61 @@ class snakeLadder{
 
 	public void diesRoll(){       //method to perform dies roll
 		int min=1;
-
-		Random r1=new Random();   // random class object has been created
-		diesRoll=min+r1.nextInt(6); //initilazing random type object using nextInt method to get random numbers from 1 to 6
-
-		System.out.println("No of position to move:"+diesRoll);
+		
+			Random r1=new Random();   // random class object has been created
+			diesRoll=min+r1.nextInt(6); //initilazing random type object using nextInt method to get random numbers from 1 to 6
+			System.out.println("//////////////");
+			System.out.println("No of position to move:"+diesRoll);
 	}
 
 
-	public void snakeOrLadder(int dies, int playerPosition){
-		Random r2=new Random(); // object creation
-		int playCheck=r2.nextInt(3); // initialization of random typle clas
-		
-		switch(playCheck)  // to check the case for snake ladder or no play options
+	public void snakeOrLadder(){
+		 int minn=1;
+		 int playerPos=0;
+		for (int i=0; newPos<100; i++)
 		{
-			case 1:
-				System.out.println("+ladder+");
-				System.out.println("dies roll-"+diesRoll);
-				System.out.println("player pos--"+playerPosition);
-				newPos=diesRoll+playerPosition;               
-				System.out.println("new position::"+newPos);
-				break;
-			case 2:
-				System.out.println("-snake-");
-				System.out.println("dies roll--"+diesRoll);
-                                System.out.println("player pos--"+playerPosition);
-                                newPos=diesRoll-playerPosition;
-                                System.out.println("new position:"+newPos);
-                                break;
-			default:
-				System.out.println("#no play#");
-				System.out.println("new position:"+newPos);
+			Random rr1=new Random();
+			int diceRoll100=minn+rr1.nextInt(6);
+			System.out.println("dice rolled in snake and ladder:"+diceRoll100);
+			
+			Random r2=new Random(); // object creation
+			int playCheck=r2.nextInt(3); // initialization of random typle clas
+			playerPos=newPos;	
+			
+			switch(playCheck)  // to check the case for snake ladder or no play options
+			{
+				case 1:
+					System.out.println("+ladder+");
+					System.out.println("dies roll="+diceRoll100);
+					System.out.println("player pos="+playerPos);
+					newPos=diceRoll100+playerPos;               
+					System.out.println("new position="+newPos);
+					System.out.println("________");
+					break;
+				case 2:
+					System.out.println("-snake-");
+					System.out.println("dies roll="+diceRoll100);
+                                	System.out.println("player pos="+playerPos);
+					if (diceRoll100>playerPos){
+						playerPos=0;
+                                		//newPos=diceRoll100-playerPos;
+					}
+					else {
+						newPos=playerPos-diceRoll100;		
+					}
+                                	System.out.println("new position="+newPos);
+					System.out.println("________");
+                                	break;
+				default:
+					System.out.println("#no play#");
+					System.out.println("new position="+playerPos);
+				        System.out.println("________");
+
+			}
 
 		}
+
 	}
-	
 
 	public static void main(String[] args)
 	{
@@ -53,9 +73,9 @@ class snakeLadder{
 
 		snakeLadder SL=new snakeLadder(); // object creation
 	
-		SL.diesRoll(); // calling  diesroll method from object
-		int dies=SL.diesRoll;
+		//SL.diesRoll(); // calling  diesroll method from object
+		//int dies=SL.diesRoll;
 		
-		SL.snakeOrLadder(dies, playerPosition);//calling the method snake and ladder
+		SL.snakeOrLadder();//calling the method snake and ladder
 	}
 }
