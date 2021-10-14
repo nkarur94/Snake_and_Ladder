1874c6b63afe67147dae6677d99079b9e451b60b
nithunkarur <rknithun@gmail.com>
2021-10-14 13:38:29 +0530

    Added: Rolling the dies to get the numbers from 1 to 6

--- snakeLadder.java
+++ snakeLadder.java
@@ -1,4 +1,12 @@
+import java.util.Random;
 class snakeLadder{
+	public void diesRoll(){
+		Random r1=new Random();
+		int min=1;
+		int diesRoll=min+r1.nextInt(6);
+		System.out.println("No of position to move:"+diesRoll);
+
+	}
 	public static void main(String[] args)
 	{
 		System.out.println("welcome to SNAKE and LADDER Game");
@@ -6,6 +14,8 @@ class snakeLadder{
 		String playerName="Nithun";
 		int playerPosition=0;
 		System.out.println("Welcome\t"+playerName+"\t*LETS START THE GAME* Your initial postion is:"+playerPosition);
-		System.out.println("---------------------------------------------------------------------------------------");
+		System.out.println("--------------------------------------------------------------------------------");
+		snakeLadder SL=new snakeLadder();
+		SL.diesRoll();
 	}
 }
